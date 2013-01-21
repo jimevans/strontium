881fbdee3cc37a18477d35cc27522984e97a1a2c
jimevans <james.h.evans.jr@gmail.com>
2013-01-21 22:35:50 +0000

    Removing unnecessary FxCop warning suppressions

--- WebDriver.Remote.Server/GlobalSuppressions.cs
+++ WebDriver.Remote.Server/GlobalSuppressions.cs
@@ -2,7 +2,6 @@
 //
 // Copyright 2010-2013 Jim Evans (james.h.evans.jr@gmail.com)
 //
-//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -25,8 +24,6 @@
 // Error List, point to "Suppress Message(s)", and click 
 // "In Project Suppression File". 
 // You do not need to add suppressions to this file manually. 
-
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1020:AvoidNamespacesWithFewTypes", Scope = "namespace", Target = "OpenQA.Selenium.Remote.Server.Loggers", Justification = "Loggers namespace should be separate, but will contain few types.")]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1031:DoNotCatchGeneralExceptionTypes", Scope = "member", Target = "OpenQA.Selenium.Remote.Server.RemoteServer.#DispatchRequest(System.Uri,System.String,System.String)", Justification = "DispatchRequest passes all exceptions back to the caller via JSON, so catching the base Exception here is okay.")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Maintainability", "CA1506:AvoidExcessiveClassCoupling", Scope = "member", Target = "OpenQA.Selenium.Remote.Server.RemoteServer.#DispatchRequest(System.Uri,System.String,System.String)", Justification = "This method has to interpret all of the given exception types differently, thus the high coupling.")]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors", Scope = "member", Target = "OpenQA.Selenium.Remote.Server.CommandHandlerFactory.#.ctor()", Justification = "It is intended that every subclass of CommandHandlerFactory override AddHandlers")]
